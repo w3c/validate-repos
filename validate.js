3c5d9c4fd1bcbe438ac27a0e2dd41324b27bce2d
Dominique Hazael-Massieux <dom@w3.org>
2017-05-06 00:08:50 +0200

    Check if content of license/contributing files match template

--- validate.js
+++ validate.js
@@ -2,6 +2,12 @@ const fetch = require("node-fetch");
 const Octokat = require("octokat");
 const config = require("./config.json");
 
+const ghBlobToString = blob => new Buffer(blob.content, 'base64').toString('utf8');
+const nlToSpace = str => str.replace(/\n/g, " ").replace(/  /g, " ").trim();
+const httpToHttps = str => str.replace(/http:\/\/www.w3.org\//g, "https://www.w3.org/");
+
+const mdMatch = (md, ref) => nlToSpace(httpToHttps(md)).indexOf(nlToSpace(ref)) !== -1;
+
 fetch("https://w3c.github.io/spec-dashboard/groups.json")
     .then(r => r.json())
     .then(groupData => {
@@ -12,15 +18,19 @@ fetch("https://w3c.github.io/spec-dashboard/groups.json")
                 results.filter(x => x).forEach(groupSpecs => Object.keys(groupSpecs).forEach(spec => repos.add(groupSpecs[spec].repo.owner + '/' + groupSpecs[spec].repo.name)));
 
                 //console.log(repos);
-
+                let contributing, contributingSw, license, licenseSw;
                 const octo = new Octokat({ token: config.ghToken });
-                const errors = {"now3cjson":[], "invalidcontacts":[], "nocontributing":[], "nolicense": [], "noreadme": []};
+                const errors = {"now3cjson":[], "invalidcontacts":[], "nocontributing":[], "invalidcontributing": [], "nolicense": [], "invalidlicense": [], "noreadme": []};
                 Promise.all([...repos].map(repofullname => {
-                    return octo
-                        .repos(...repofullname.split('/'))
-                        .contents('w3c.json').fetch()
-                        .then(function(w3cinfodesc) {
-                            var w3cinfo = JSON.parse(new Buffer(w3cinfodesc.content, 'base64').toString('utf8'));
+                    return octo.repos('w3c/licenses').contents('WG-CONTRIBUTING.md').fetch().then(ghBlobToString).then(text => contributing = text)
+                        .then(() => octo.repos('w3c/licenses').contents('WG-CONTRIBUTING-SW.md').fetch().then(ghBlobToString).then(text => contributingSw = text))
+                        .then(() => octo.repos('w3c/licenses').contents('WG-LICENSE.md').fetch().then(ghBlobToString).then(text => license = text))
+                        .then(() => octo.repos('w3c/licenses').contents('WG-LICENSE-SW.md').fetch().then(ghBlobToString).then(text => licenseSw = text))
+                        .then(() =>
+                              octo.repos(...repofullname.split('/'))
+                              .contents('w3c.json').fetch())
+                        .then(ghBlobToString)
+                        .then(function(w3cinfo) {
                             return Promise.all(w3cinfo.contacts.map(function(username) {
                                 if (typeof username !== "string") {
                                     errors.invalidcontacts.push({repo: repofullname, value: username});
@@ -37,20 +47,24 @@ fetch("https://w3c.github.io/spec-dashboard/groups.json")
                             }));
                         }).catch(() => errors.now3cjson.push(repofullname))
                             .then(() => octo.repos(...repofullname.split('/'))
-                                  .contents('CONTRIBUTING.md').fetch())
-                        .then(() => {
-                            // test content
-                        }).catch(() => errors.nocontributing.push(repofullname))
-                            .then(() => octo.repos(...repofullname.split('/'))
-                                  .contents('LICENSE.md').fetch())
-                        .then(() => {
-                            // test content
-                        }).catch(() => errors.nolicense.push(repofullname))
-                            .then(() => octo.repos(...repofullname.split('/'))
-                                  .contents('README.md').fetch())
-                        .then(() => {
-                            // test content
-                        }).catch(() => errors.noreadme.push(repofullname));
+                                  .contents('CONTRIBUTING.md').fetch()
+                                  .then(ghBlobToString)
+                                  .then((repoContributing) => {
+                                      if (!mdMatch(repoContributing, contributing) && !mdMatch(repoContributing,contributingSw)) errors.invalidcontributing.push({repo: repofullname, contributing: repoContributing});
+                                  }, () => errors.nocontributing.push(repofullname)))
+                        .then(() => octo.repos(...repofullname.split('/'))
+                              .contents('LICENSE.md').fetch()
+                              .then(ghBlobToString)
+                              .then((repoLicense) => {
+                                  if (!mdMatch(repoLicense, license) && !mdMatch(repoLicense, licenseSw)) errors.invalidlicense.push({repo: repofullname, license: repoLicense});
+
+                              }, (err) => errors.nolicense.push(repofullname)))
+                        .then(() => octo.repos(...repofullname.split('/'))
+                              .contents('README.md').fetch()
+                              .then(ghBlobToString)
+                              .then(() => {
+                                  // test content
+                              }, () => errors.noreadme.push(repofullname)));
                 })).then(() => console.log(JSON.stringify(errors,null,2)));
             });
     });
