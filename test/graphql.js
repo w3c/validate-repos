cb25d5ec80da42f6afea681f66c302dbd9044b5f
Philip Jägenstedt <philip@foolip.org>
2019-12-13 10:27:15 +0100

    Use @octokit/graphql instead of own bindings
    This means a bit less code to maintain, and the ability to use GraphQL
    variables.
    
    lib/graphql.js is kept around to make it easier to mock out including
    the config in other tests.
    
    Also normalize the indentation of the GraphQL query blocks.

--- test/graphql.js
+++ test/graphql.js
@@ -4,92 +4,47 @@
 
 const assert = require('assert');
 const proxyquire = require('proxyquire');
-const sinon = require('sinon');
 
-const GH_API = 'https://api.github.com/graphql';
+// Rather than mocking out the whole @octokit/graphql dependency its internal
+// fetch is replaced, see ttps://github.com/octokit/graphql.js/#writing-tests
+const graphql = proxyquire('../lib/graphql.js', {
+  '../config.json': {ghToken: 'mock-token'},
+});
 
 describe('graphql', () => {
-  const mockConfig = {ghToken: 'mock-token'};
-
   it('happy path', async () => {
-    const fakeFetch = sinon.fake.resolves({
-      async json() {
-        return {data: 'mock response'}
+    const res = await graphql('mock query', {
+      request: {
+        fetch: async (url, options) => {
+          assert.equal(options.headers.authorization, 'token mock-token');
+          assert.equal(options.body, '{"query":"mock query"}');
+          return {
+            headers: new Map([['content-type', 'application/json']]),
+            async json() {
+              return {data: 'mock response'};
+            }
+          }
+        }
       }
     });
-    const graphql = proxyquire('../lib/graphql.js', {
-      '../config.json': mockConfig,
-      'node-fetch': fakeFetch,
-    });
-    const res = await graphql('mock query');
     assert.equal(res, 'mock response');
-    assert(fakeFetch.calledOnce);
-    const args = fakeFetch.args[0];
-    assert.equal(args[0], GH_API);
-    assert.equal(args[1].method, 'POST');
-    assert.equal(args[1].headers['Authorization'], 'bearer mock-token');
-    assert.equal(args[1].body, '{"query":"mock query"}');
   });
 
-  it('API error', async () => {
-    const fakeFetch = sinon.fake.resolves({
-      async json() {
-        return {errors: [{message: 'API error'}]}
+  it('error path', async () => {
+    await assert.rejects(graphql('mock query', {
+      request: {
+        fetch: async () => {
+          return {
+            headers: new Map([['content-type', 'application/json']]),
+            async json() {
+              return {errors: [{message: 'mock error'}]};
+            }
+          }
+        }
       }
-    });
-    const graphql = proxyquire('../lib/graphql.js', {
-      '../config.json': mockConfig,
-      'node-fetch': fakeFetch,
-    });
-    await assert.rejects(graphql('mock query'), {
-      name: 'Error',
-      message: 'API error'
+    }), {
+      name: 'GraphqlError',
+      message: 'mock error'
     });
   });
-
-  it('multiple API errors', async () => {
-    const mockErrors = [
-      {message: 'message 1', type: 'type 1'},
-      {message: 'message 2', type: 'type 2'},
-    ];
-    const fakeFetch = sinon.fake.resolves({
-      async json() {
-        return {errors: mockErrors}
-      }
-    });
-    const graphql = proxyquire('../lib/graphql.js', {
-      '../config.json': mockConfig,
-      'node-fetch': fakeFetch,
-    });
-    let err;
-    try {
-      await graphql('mock query');
-    } catch (e) {
-      err = e;
-    }
-    assert(err, 'no error thrown');
-    assert.strictEqual(err.message, 'message 1');
-    assert.strictEqual(err.type, 'type 1');
-    assert.deepStrictEqual(err.all, mockErrors);
-  });
-
-  it('fetch error', async () => {
-    const fakeFetch = sinon.fake.resolves({
-      type: 'error',
-      status: 500,
-      ok: false,
-      async json() {
-        return {data: 'valid response'}
-      }
-    });
-    const graphql = proxyquire('../lib/graphql.js', {
-      '../config.json': mockConfig,
-      'node-fetch': fakeFetch,
-    });
-    // fetch error isn't handled and if the response is JSON it's used.
-    const res = await graphql('mock query');
-    assert.equal(res, 'valid response');
-  });
-
-  afterEach(() => sinon.restore());
 });
