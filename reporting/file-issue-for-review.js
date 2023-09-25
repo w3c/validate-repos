939a21fdc5b736be17f06c744b4a3c1766cafc66
Dominique Hazael-Massieux <dom@w3.org>
2023-09-25 14:05:02 +0200

    Fix code typo

--- reporting/file-issue-for-review.js
+++ reporting/file-issue-for-review.js
@@ -32,7 +32,7 @@ function issueWrapper(anomalies, anomalyType) {
     break;
   case 'inconsistentgroups':
     title = `Inconsistent repo information on IPR`;
-    anomalyReport = 'The w3c.json file in this repo indicates a different W3C group from the one associated to it in the [IPR checker](https://labs.w3.org/repo-manager/);
+    anomalyReport = 'The w3c.json file in this repo indicates a different W3C group from the one associated to it in the [IPR checker](https://labs.w3.org/repo-manager/);'
     break;
 
   }
